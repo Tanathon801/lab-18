61f82eeeaf81c9dbf73bb84453f8fe518c527dd8
Tanathon801 <aumatomic@gmail.com>
2025-02-11 16:11:05 +0700

    Update lab18_1.cpp

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -5,14 +5,15 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(Rect *r1, Rect *r2){
-	r1->w = r1->x + r1->w;
-	r1->h = r1->y - r1->h;
+double overlap(Rect r1,Rect r2){
+    
+    r1.w = r1.x + r1.w;
+	r1.h = r1.y - r1.h;
 
-	r2->w = r2->x + r2->w;
-	r2->h = r2->y - r2->h;
+	r2.w = r2.x + r2.w;
+	r2.h = r2.y - r2.h;
 
-	double width = min(r1->w, r2->w) - max(r1->x, r2->x);
-	double heigth = min(r1->y, r2->y) - max(r1->h, r2->h);
+	double width = min(r1.w, r2.w) - max(r1.x, r2.x);
+	double heigth = min(r1.y, r2.y) - max(r1.h, r2.h);
 	return max(0.0, width) * max(0.0, heigth);
 }
